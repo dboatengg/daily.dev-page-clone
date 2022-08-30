0d6c35c2f3e1741cacdc0b5c74afc34c65109a3a
dboatengg <dicksonboateng67@gmail.com>
2022-08-30 09:06:20 +0000

    Add project colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
+    colors: {
+      background: "#0e1217",
+      "scroll-background": "hsla(0, 0%, 86.7%, 0)",
+      "white-color": "#fff",
+      "button-color": "#151618",
+      "body-text": "#a9abb3",
+      pink: "#d74cf6",
+      "blue-ish": "#208bff",
+      "gradient-red": "#f5005e",
+      "gradient-blue": "#4801f5",
+      green: "#00f576",
+    },
     extend: {},
   },
   plugins: [],
